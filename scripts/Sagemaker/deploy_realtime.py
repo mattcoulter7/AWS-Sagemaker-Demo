49c2ce358585c93c767c09b83d366901c0315b8c
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2023-01-27 10:20:38 +0800

    Update deploy_realtime.py

--- scripts/Sagemaker/deploy_realtime.py
+++ scripts/Sagemaker/deploy_realtime.py
@@ -8,18 +8,23 @@ hub = {
 	'HF_TASK':'text2text-generation'
 }
 
+model_name = 't5-base-qa-qg-qg'
+endpoint_name = f'{model_name}-realtime'
+
 # create Hugging Face Model Class
 huggingface_model = HuggingFaceModel(
 	transformers_version='4.17.0',
 	pytorch_version='1.10.2',
 	py_version='py38',
 	env=hub,
-	role=role, 
+	role=role,
+    name=model_name
 )
 # deploy model to SageMaker Inference
 predictor = huggingface_model.deploy(
 	initial_instance_count=1, # number of instances
-	instance_type='ml.m5.xlarge' # ec2 instance type
+	instance_type='ml.m5.xlarge', # ec2 instance type
+    endpoint_name=endpoint_name
 )
 
 output = predictor.predict({
